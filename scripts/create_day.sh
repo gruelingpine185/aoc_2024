082a8e537aa7f4d2352c959ea5236ba680a06293
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2024-12-01 07:11:13 -0500

    removed second argument

--- scripts/create_day.sh
+++ scripts/create_day.sh
@@ -2,21 +2,12 @@
 
 create_day() {
   local day="$1"
-  local part="$2"
 
   printf -v padded_day "%02d" "$day"
   if [[ ! -e "./days/${padded_day}" ]]; then
     mkdir -p "days/${padded_day}"
   fi
 
-  if [[ -e "/days/${padded_day}/input_2.txt" ]]; then
-    echo "Day ${day} files already downloaded and present at ./days/${padded_day}/"
-    exit 0
-  elif [[ -e "/days/${padded_day}/input_1.txt" ]]; then
-    part="2"
-  else
-    part="1"
-  fi
 
   if [[ -z "$AOC_SESSION" ]]; then
     echo "Please export your AoC session"
@@ -34,15 +25,21 @@ create_day() {
     hxselect -c -s "\n\n" p, pre | \
     fold -s > "days/${padded_day}/prompt.md"
 
-  # download puzzle input
-  curl --cookie session=$(printenv AOC_SESSION) "${aoc_url}/input" \
-    > "days/${padded_day}/input_${part}.txt"
+  if [[ -e "/days/${padded_day}/input.txt" ]]; then
+    echo "Day ${day} files already downloaded and present at ./days/${padded_day}/"
+    exit 0
+  else
+    # download puzzle input
+    curl --cookie session=$(printenv AOC_SESSION) "${aoc_url}/input" \
+      > "days/${padded_day}/input.txt"
+  fi
 }
 
 if [[ $# != 2 ]]; then
   echo "create_day.sh <day> <part>"
+if [[ $# != 1 ]]; then
   exit 0
 fi
 
-create_day "$1" "$2"
+create_day "$1"
 exit 0
