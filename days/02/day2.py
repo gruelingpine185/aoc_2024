e074d9b0dc5def2c2361d066bbca88001ed436cd
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2024-12-02 11:06:02 -0500

    part 1

---
+++ days/02/day2.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Expected <input file>")
+        exit(0)
+
+    levels, safe_levels, unsafe_levels = extract_levels(sys.argv[1])
+    print(f"part 1: {part_1(safe_levels)}")
+
+def cmp(l, r):
+    if l == r:
+        return 0
+    elif l > r:
+        return -1
+    else:
+        return 1
+
+def validate(dir, l, r):
+    if dir == 0:
+        return False
+    elif cmp(l, r) != dir:
+        return False
+    elif abs(l - r) > 3:
+        return False
+
+    return True
+
+def is_safe_level(level):
+    l = int(level[0])
+    r = int(level[1])
+    dir = cmp(l, r)
+    if not validate(dir, l, r):
+        return 1
+
+    is_safe = True
+    for i in range(1, len(level) - 1):
+        l = int(level[i])
+        r = int(level[i + 1])
+        if not validate(dir, l, r):            
+            return i
+
+    if is_safe == True:
+        return len(level)
+
+def extract_levels(pathname):
+    levels = []
+    safe_levels = []
+    unsafe_levels = []
+    with open(pathname) as file:
+        for line in file:
+            levels.append(line.split("\n")[0].split(" "))
+
+    for level in levels:
+        err_idx = is_safe_level(level)
+        if err_idx != len(level):
+            unsafe_levels.append([err_idx, level])
+        else:
+            safe_levels.append(level)
+
+    return levels, safe_levels, unsafe_levels
+
+def part_1(safe_levels):
+    return len(safe_levels)
+
+main()
